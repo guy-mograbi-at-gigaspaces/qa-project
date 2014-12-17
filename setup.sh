04d0fddb863770708fdae7d00583064964852c4d
guym <guym@gigaspaces.com>
2014-12-17 17:57:34 +0200

    updated labels for test units.

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ echo "making sure global libraries exist"
 ( `which grunt` && echo "grunt exists" ) || (echo "installing grunt" &&  sudo npm install -g grunt-cli )
 ( `which bower` && echo "bower exists" ) || (echo "installing bower" && sudo npm install -g bower )
 ( `which compass` && echo "compass exists" )  || ( echo "installing compass" && sudo gem install compass )
-
+sudo npm cache clean
 echo "installing dependencies" && npm install
 echo "installing front-end dependencies" && bower install
 echo "compiling css" && grunt
